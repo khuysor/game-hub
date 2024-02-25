d3148620eafc154b16b25c9fe2610a8406374698
Huysorkheang <xain@Huysorkheangs-MacBook-Pro.local>
2024-02-25 22:05:52 +0700

    create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo/OIG.jpeg";
+function Navbar() {
+  return (
+    <HStack>
+      <Image src={logo} boxSize={"60px"} />
+      <Text>NavBar </Text>
+    </HStack>
+  );
+}
+
+export default Navbar;
